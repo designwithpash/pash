a803e2018b2f83646e5d5f5aae27349620a81d01
Pash <designwithpash@gmail.com>
2025-03-21 11:18:02 +0000

    server

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.get("/", (req, res) => {
 
 // Start server
 app.listen(3000, () => {
-    console.log("Listening on http://localhost:3000");
+    console.log("Listening on http://0.0.0.0:3000");
 });
 
